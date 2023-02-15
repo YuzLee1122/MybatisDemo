731124ef1bbe08f7857eea356d11c56e8b8dc4ca
marry <atguigutest@163.com>
2023-02-15 14:36:45 +0800

    springboot 1

---
+++ springbootdemo/src/main/java/com/atguigu/springbootdemo/SpringbootdemoApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//主启动类 App的启动入口
+@SpringBootApplication
+public class SpringbootdemoApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdemoApplication.class, args);
+    }
+
+}
